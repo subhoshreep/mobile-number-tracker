62fc513648b99063b33fe6d893ce6c0078ea517c
SUBHOSHREE PATTNAIK <76872340+subhoshreep@users.noreply.github.com>
2021-03-19 22:18:40 +0530

    Add files via upload

---
+++ mobiletrack.py
@@ -0,0 +1,9 @@
+import phonenumbers
+from test import number
+
+from phonenumbers import geocoder
+ch_nmber = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_nmber, "en"))
+from phonenumbers import carrier
+servic_num=phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(servic_num, "en"))
